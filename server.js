bada1e93b1baee0f43cfa89973c795ef2d138975
Raimundo Sotomayor <raimundo.sotomayor@gmail.com>
2026-07-26 13:33:04 -0400

    Diagnostico temporal: largo/prefijo de GITHUB_TOKEN

--- server.js
+++ server.js
@@ -458,4 +458,7 @@ server.listen(PORT, () => {
     console.log('Para usarla desde el celular, busca la IP local de este PC (ipconfig) y entra a http://<esa-IP>:' + PORT);
   }
   if (process.env.APP_PIN) console.log('PIN requerido para /api/*.');
+  if (process.env.GITHUB_TOKEN) {
+    console.log(`[diagnostico] GITHUB_TOKEN largo=${process.env.GITHUB_TOKEN.length} prefijo=${process.env.GITHUB_TOKEN.slice(0, 12)} sufijo=${process.env.GITHUB_TOKEN.slice(-4)}`);
+  }
 });
